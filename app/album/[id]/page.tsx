57a189b0588bfc4df1c50300bfd483f40fe6e11f
robertinho_programador <guilhermemartinez39@gmail.com>
2025-09-29 21:08:09 -0300

    Organize album page using Prettier

--- app/album/[id]/page.tsx
+++ app/album/[id]/page.tsx
@@ -1,16 +1,30 @@
-import { fetchAlbumCredits, fetchAlbumSongs } from '@/lib/server-fetching';
-import { notFound } from 'next/navigation';
-import AlbumPageRootComponent from '@/components/albumpageRoot';
-import { AlbumPageResponse, CreditsResponse } from '@/lib/interfaces';
+import { fetchAlbumCredits, fetchAlbumSongs } from "@/lib/server-fetching";
+import { notFound } from "next/navigation";
+import AlbumPageRootComponent from "@/components/albumpageRoot";
+import { AlbumPageResponse, CreditsResponse } from "@/lib/interfaces";
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
 
-  const data: AlbumPageResponse = await fetchAlbumSongs(id.toLowerCase().replace(" ", "-"));
-  const credits: CreditsResponse = await fetchAlbumCredits(id.toLowerCase().replace(" ", "-"));
+    const data: AlbumPageResponse = await fetchAlbumSongs(
+        id.toLowerCase().replace(" ", "-")
+    );
+    const credits: CreditsResponse = await fetchAlbumCredits(
+        id.toLowerCase().replace(" ", "-")
+    );
 
-  if (data === "NOT FOUND") return notFound();
+    if (data === "NOT FOUND") return notFound();
 
-  //@ts-expect-error
-  return <AlbumPageRootComponent data={data} albumCreditData={credits.credits} id={id.toLowerCase().replace(" ", "-")} />
+    return (
+        <AlbumPageRootComponent
+            data={data}
+            //@ts-expect-error
+            albumCreditData={credits.credits}
+            id={id.toLowerCase().replace(" ", "-")}
+        />
+    );
 }
